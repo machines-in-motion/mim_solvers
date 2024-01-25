395892d42f700605252bd38c4992fc6c3869b3b3
Armand Jordana <aj2988@nyu.edu>
2024-01-25 09:28:33 -0500

    fixed cmake

--- benchmarks/timings.cpp
+++ benchmarks/timings.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
-#include "mim_solvers/timings.hpp"
+// #include <iostream>
+// #include "timings.hpp"
 
-int main(){
-    std::cout << "Hello world" << std::endl;
-    mim_solvers::Timer timer;
-    timer.start();
-    timer.stop();
-    std::cout << timer.elapsed().user << std::endl;
-    return 0;
-}
+// int main(){
+//     std::cout << "Hello world" << std::endl;
+//     mim_solvers::Timer timer;
+//     timer.start();
+//     timer.stop();
+//     std::cout << timer.elapsed().user << std::endl;
+//     return 0;
+// }
