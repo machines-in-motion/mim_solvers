dd4ab294d5dfb75ffd823586001b697a6de8e181
Maximilien Naveau <maximilien.naveau@gmail.com>
2025-03-18 09:28:11 +0100

    Add nix derivation

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "ROS integration for Franka research robots";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    crocoddyl = {
+      # Devel branch because boost::shared_ptr -> std::shared_ptr
+      url = "github:loco-3d/crocoddyl/devel";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+      perSystem =
+        { pkgs, self', ... }:
+        {
+          apps.default = {
+            type = "app";
+            program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
+          };
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ self'.packages.default ];
+            packages = [ (pkgs.python3.withPackages (p: [p.tomlkit])) ]; # for "make release"
+          };
+          packages = {
+            default = self'.packages.mim-solvers;
+            mim-solvers = pkgs.python3Packages.mim-solvers.overrideAttrs (_: {
+              src = pkgs.lib.fileset.toSource {
+                root = ./.;
+                fileset = pkgs.lib.fileset.unions [
+                  ./benchmark
+                  ./bindings
+                  ./examples
+                  ./include
+                  ./python
+                  ./src
+                  ./tests
+                  ./CMakeLists.txt
+                  ./package.xml
+                ];
+              };
+            });
+          };
+        };
+    };
+}
